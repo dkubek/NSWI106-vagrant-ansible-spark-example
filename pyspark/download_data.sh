3b57b064b1db967c40abd9b3e17b22b278c80874
mpicek <martinpice4@gmail.com>
2021-03-03 10:24:55 +0100

    download_data script

---
+++ pyspark/download_data.sh
@@ -0,0 +1,5 @@
+#!/usr/bin/env bash
+
+wget https://raw.githubusercontent.com/selva86/datasets/master/BostonHousing.csv
+wget https://pjreddie.com/media/files/mnist_train.csv
+wget https://pjreddie.com/media/files/mnist_test.csv
